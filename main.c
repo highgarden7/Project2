0a4b21ba1ccaa821637136beeb7aaf22c5ea149e
jaeen1113 <jaeen1113@naver.com>
2018-04-28 15:55:58 +0900

    time compare(min/hour, day/month) code add

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 
 struct stat stat1, stat2;
 struct tm *time1, *time2;
+int mon1, mon2, d1, d2;
+int h1, h2, min1, min2;
 
 void filestat1(void);
 void filestat2(void);
@@ -61,9 +63,36 @@ void blockcmp(void)
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(void)
 {
+    int result = 0;
+
+    if(d1 < d2) result = 1;
+    if(d1 > d2) result = 2;
+    if(mon1 < mon2) result=1;
+    if(mon1 > mon2) result=2;
+
+    if(result ==0)
+	printf("same time\n");
+    else if(result==1)
+	printf("text1 is early\n");
+    else
+	printf("text2 is early\n");
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(void)
 {
+    int result = 0;
+
+    if(min1 < min2) result = 1;
+    if(min1 > min2) result = 2;
+    if(h1 < h2) result=1;
+    if(h1 > h2) result=2;
+
+    if(result ==0)
+	printf("same time\n");
+    else if(result==1)
+	printf("text1 is early\n");
+    else
+	printf("text2 is early\n");
+
 }
